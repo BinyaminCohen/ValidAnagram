0078a0e83c2e3c763e0ea41928d4e73f99ba0635
BinyaminPythonProblem <Binyamin.cohen111@gmail.com>
2024-01-31 19:09:01 +0200

    Given two strings s and t, return true if t is an anagram of s, and false otherwise.

---
+++ ValidAnagram.py
@@ -0,0 +1,23 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+class ValidAnagram(object):
+    def is_anagram(self, s, t):
+
+        if len(s) != len(t):
+            print(f"The string {t} isn't anagram.")
+
+        else:
+            count_letter = 0
+            for s_char in s:
+                if s_char.strip() not in t.strip():
+                    print(f"The string {t} isn't anagram.")
+                    return None
+            print(f"The string {t} is anagram.")
+
+
+ValidAnagram().is_anagram("anagram", "nagaram")
+ValidAnagram().is_anagram("rat", "car")
+ValidAnagram().is_anagram("", "")
+
+
+
